dafee23f7d7c157b59dc617a8d3b1639c6275bcf
ankushlohiya001 <ankushlohiya2000@gmail.com>
2023-02-07 16:01:38 +0530

    removed unwanted vector

--- src/reading.rs
+++ src/reading.rs
@@ -9,17 +9,14 @@ pub trait Reading {
         self.read_line().parse().unwrap_or(T::default())
     }
 
-    fn read_vec<T>(&mut self, size: Option<usize>) -> Vec<T>
+    fn read_vec<T>(&mut self) -> Vec<T>
     where
         T: FromStr + Default,
     {
         let txt = self.read_line();
         let wrds = txt.split_whitespace();
-        let mut vc = Vec::with_capacity(size.unwrap_or(0));
-        for wrd in wrds {
-            let val = wrd.parse().unwrap_or(T::default());
-            vc.push(val);
-        }
-        vc
+        wrds
+            .map(|wrd| wrd.parse().unwrap_or(T::default()))
+            .collect()
     }
 }
